148aa8fe3bcde516316ace02051c66ca11238eca
Prof. Celso <celsofurtado@terra.com.br>
2021-04-23 11:04:01 -0300

    Aula Turma B 23-04-2021

--- app/src/main/java/br/senai/sp/jandira/imc_app_b/MainActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/imc_app_b/MainActivity.kt
@@ -1,11 +1,25 @@
 package br.senai.sp.jandira.imc_app_b
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.cardview.widget.CardView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // ** Clique no card IMC
+        val cardImc = findViewById<CardView>(R.id.card_imc)
+
+        cardImc.setOnClickListener {
+
+            val abrirImcActivity = Intent(this, ImcActivity::class.java)
+            startActivity(abrirImcActivity)
+
+        }
+
     }
 }
